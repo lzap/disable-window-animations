a75c6782934304a88dd45ae350c0486ab60ad202
Lukas Zapletal <lzap+git@redhat.com>
2011-07-21 18:00:27 +0200

    indenting according to code requirements

--- extension.js
+++ extension.js
@@ -1,11 +1,10 @@
-const St = imports.gi.St;
-const Mainloop = imports.mainloop;
-
 const Main = imports.ui.main;
 
 function main() {
-  // monkey patch the should animate function in the wm
-  Main.wm._shouldAnimate = function(actor) {
-    return false;
-  }
+    // monkey patch the should animate function in the wm
+    Main.wm._shouldAnimate = function(actor) {
+        return false;
+    }
 }
+
+// vim:sw=4:ts=4:et:
