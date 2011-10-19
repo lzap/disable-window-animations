2e0679789d0b4e93aa6ff5abf25e1293d75d4607
Ole Martin Bjørndalen <ombdalen@gmail.com>
2011-10-19 20:07:24 +0200

    Updated extension to work with gnome-shell 3.2.

--- extension.js
+++ extension.js
@@ -1,10 +1,25 @@
 const Main = imports.ui.main;
 
-function main() {
+let oldShouldAnimate;
+
+function init() {
+    oldShouldAnimate = Main.wm._shouldAnimate;
+}
+
+function enable() {
     // monkey patch the should animate function in the wm
     Main.wm._shouldAnimate = function(actor) {
         return false;
     }
+}
+
+function disable() {
+    Main.wm._shouldAnimate = oldShouldAnimate;
+}
+
+// Backwards compatability with 3.0.2
+function main() {
+    enable();
 }
 
 // vim:sw=4:ts=4:et:
